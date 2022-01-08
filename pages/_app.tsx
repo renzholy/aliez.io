4e81e9f368e810e9f912288b7a9ff394fe8f989d
Found Pan Tiger <renzholy@gmail.com>
2022-01-08 20:28:36 +0800

    add image

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,54 @@
-import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
+
+import "../styles/globals.css";
+
+const TITLE = "A.L.I.E.Z";
+
+const DESCRIPTION = "a naive developer.";
+
+const BASE_URL = "https://aliez.io/";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta httpEquiv="x-dns-prefetch-control" content="on" />
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
+        />
+        <link
+          rel="icon"
+          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👾</text></svg>"
+        />
+        <title>{TITLE}</title>
+        <meta name="description" content={DESCRIPTION} />
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:url" content={BASE_URL} />
+        <meta name="twitter:title" content={TITLE} />
+        <meta name="twitter:description" content={DESCRIPTION} />
+        <meta
+          key="twitter:image"
+          name="twitter:image"
+          content={`${BASE_URL}image.png`}
+        />
+        <meta name="twitter:creator" content="@aliez_eth" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content={TITLE} />
+        <meta property="og:description" content={DESCRIPTION} />
+        <meta property="og:site_name" content={TITLE} />
+        <meta property="og:url" content={BASE_URL} />
+        <meta
+          key="og:image"
+          property="og:image"
+          content={`${BASE_URL}image.png`}
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,4 @@
 import type { NextPage } from "next";
-import Head from "next/head";
 import Image from "next/image";
 
 import styles from "../styles/Home.module.css";
@@ -7,15 +6,6 @@ import styles from "../styles/Home.module.css";
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
-      <Head>
-        <title>A.L.I.E.Z</title>
-        <meta name="description" content="A-Z Looser-Krankheit-Was IS das?" />
-        <link
-          rel="icon"
-          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👾</text></svg>"
-        />
-      </Head>
-
       <main className={styles.main}>
         <h1 className={styles.title}>A.L.I.E.Z</h1>
 
